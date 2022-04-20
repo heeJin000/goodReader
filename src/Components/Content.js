28341bb79dd0fc89ddaa779af0366c3b615fcec8
박희진 <heejinpark10@gmail.com>
2022-04-20 22:40:28 +0900

    component commit

---
+++ src/Components/Content.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Content extends Component{
+    render(){
+        return(
+            <header>
+                <p> row </p>
+            </header>
+        );
+    }
+}
+export default Content;
---
+++ src/Components/Title.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Title extends Component{
+    render(){
+      return(
+        <header>
+          <h1>GoodReader</h1>     
+        </header>
+      );
+    }
+  }
+
+export default Title;
