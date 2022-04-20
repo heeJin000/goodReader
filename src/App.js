1d593f1dc41dfd9bad260e69f5f4191efb470bdc
박희진 <heejinpark10@gmail.com>
2022-04-20 22:40:28 +0900

    commit component

--- src/App.js
+++ src/App.js
@@ -1,66 +1,24 @@
-<<<<<<< HEAD
 import React, {Component} from 'react';
 import Title from "./Components/Title";
 import Content from "./Components/Content.js";
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import ApiCall from "./Components/ApiCall.js";
 import TestFunction from "./Components/TestFunction.js";
-=======
->>>>>>> 41a79c4a (first commit)
-=======
->>>>>>> 28341bb7 (component commit)
-=======
-import TestFunction from "./Components/TestFunction.js";
->>>>>>> b02149a2 (change to function form)
-=======
-import ApiCall from "./Components/ApiCall.js";
->>>>>>> 8f72024c (api)
 import './App.css';
 import { render } from 'react-dom';
 
 const App = () => {
   return (
     <div className="App">
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 28341bb7 (component commit)
       
         <Title></Title>        
       
         <Content></Content>
 
         <Content></Content>
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 
         <TestFunction></TestFunction>
 
-=======
-      <header className="App-header">
-        
-        <p>
-          참 잘 읽었어요! Good Job!
-          
-        </p>
-        
-      </header>
->>>>>>> 41a79c4a (first commit)
-=======
-      
-=======
-
-<<<<<<< HEAD
-        <TestFunction></TestFunction>
->>>>>>> b02149a2 (change to function form)
-=======
-        <ApiCall></ApiCall>
->>>>>>> 8f72024c (api)
-
->>>>>>> 28341bb7 (component commit)
     </div>
   );
 }
--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,31 +1,10 @@
 import React, {Component} from 'react';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 const Content = () => {
     return(
             <header>
                 <p> row </p>
             </header>
     );
-=======
-class Content extends Component{
-    render(){
-        return(
-            <header>
-                <p> row </p>
-            </header>
-        );
-    }
->>>>>>> 28341bb7 (component commit)
-=======
-const Content = () => {
-    return(
-            <header>
-                <p> row </p>
-            </header>
-    );
-    
->>>>>>> b02149a2 (change to function form)
 }
 export default Content;
