4e399ff27509ad75d3fee333faabe2069b555cc1
박희진 <heejinpark10@gmail.com>
2022-04-20 22:40:28 +0900

    please commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@
 import React, {Component} from 'react';
 import Title from "./Components/Title";
 import Content from "./Components/Content.js";
+<<<<<<< HEAD
 import ApiCall from "./Components/ApiCall.js";
 import TestFunction from "./Components/TestFunction.js";
 =======
 >>>>>>> 41a79c4a (first commit)
+=======
+>>>>>>> 28341bb7 (component commit)
 import './App.css';
 import { render } from 'react-dom';
 
@@ -13,12 +16,16 @@ const App = () => {
   return (
     <div className="App">
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 28341bb7 (component commit)
       
         <Title></Title>        
       
         <Content></Content>
 
         <Content></Content>
+<<<<<<< HEAD
 
 
         <TestFunction></TestFunction>
@@ -33,6 +40,10 @@ const App = () => {
         
       </header>
 >>>>>>> 41a79c4a (first commit)
+=======
+      
+
+>>>>>>> 28341bb7 (component commit)
     </div>
   );
 }
--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 
+<<<<<<< HEAD
 const Content = () => {
     return(
             <header>
                 <p> row </p>
             </header>
     );
+=======
+class Content extends Component{
+    render(){
+        return(
+            <header>
+                <p> row </p>
+            </header>
+        );
+    }
+>>>>>>> 28341bb7 (component commit)
 }
 export default Content;
