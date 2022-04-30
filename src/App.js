bfc3aac04c5055561afa0ac5e6148654eb07c138
박희진 <heejinpark10@gmail.com>
2022-04-30 23:15:59 +0900

    11:15 commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Title from "./Components/Title";
 import Content from "./Components/Content.js";
-import TestFunction from "./Components/TestFunction.js";
+import ApiCall from "./Components/ApiCall.js";
 import './App.css';
 import { render } from 'react-dom';
 
@@ -15,7 +15,7 @@ const App = () => {
 
         <Content></Content>
 
-        <TestFunction></TestFunction>
+        <ApiCall></ApiCall>
 
     </div>
   );
