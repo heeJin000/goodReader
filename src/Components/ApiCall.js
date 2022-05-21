840c2ff040af3dafdd674e03d86f81c8e9f7deea
박희진 <heejinpark10@gmail.com>
2022-05-21 23:20:45 +0900

    api commit

--- src/Components/ApiCall.js
+++ src/Components/ApiCall.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, {Component} from 'react';
 
-<<<<<<< HEAD
 
 const ApiCall = () => {
   axios.defaults.withCredentials = true;
@@ -16,13 +15,6 @@ const ApiCall = () => {
           });
     }  
 
-=======
-const ApiCall = () => {
-    
-      const response = axios.get('https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbtogether0002243001&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101');
-      console.log(response.data)    
-  }
->>>>>>> 8f72024c (api)
 
 
 export default ApiCall;
