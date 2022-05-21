e2a96d4cf6eff864f6d78ad8079cfe2aba33af75
박희진 <heejinpark10@gmail.com>
2022-05-21 23:20:45 +0900

    404 error

--- src/Components/ApiCall.js
+++ src/Components/ApiCall.js
@@ -1,11 +1,21 @@
 import axios from 'axios';
 import React, {Component} from 'react';
 
+
+axios.defaults.withCredentials = true;
 const ApiCall = () => {
     
-      const response = axios.get('https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbtogether0002243001&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101');
-      console.log(response.data)    
-  }
+      const response = axios.post(
+        '${http://localhost:3000/}/${https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbtogether0002243001&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101}'
+        ,{},
+        {
+          withCredentials:true  // 쿠키 cors 통신 설정
+      }).then(response => {
+                  console.log(response);
+      });
+      
+    }  
+
 
 
 export default ApiCall;
