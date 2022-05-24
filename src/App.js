07448dc81488ba822d1ac8e1626d42f6395dec61
박희진 <heejinpark10@gmail.com>
2022-05-24 22:50:25 +0900

    main

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import Title from "./Components/Title";
+import Content from "./Components/Content.js";
+import ApiCall from "./Components/ApiCall.js";
+import './App.css';
+import { render } from 'react-dom';
+
+const App = () => {
+  return (
+    <div className="App">
+      
+        <Title></Title>        
+      
+        <Content></Content>
+
+        <Content></Content>
+
+        <ApiCall></ApiCall>
+
+    </div>
+  );
+}
+export default App;
---
+++ src/Components/Content.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+const Content = () => {
+    return(
+            <header>
+                <p> row </p>
+            </header>
+    );
+    
+}
+export default Content;
