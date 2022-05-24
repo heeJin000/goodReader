d0229bef22a7c2cd78192d03f2d00edcb3717032
박희진 <heejinpark10@gmail.com>
2022-05-24 22:26:27 +0900

    CORS

--- src/Components/ApiCall.js
+++ src/Components/ApiCall.js
@@ -2,18 +2,17 @@ import axios from 'axios';
 import React, {Component} from 'react';
 
 
-axios.defaults.withCredentials = true;
 const ApiCall = () => {
+  axios.defaults.withCredentials = true;
     
       const response = axios.post(
-        '${http://localhost:3000/}/${https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbtogether0002243001&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101}'
-        ,{},
+        'http://localhost:3000/', 'https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbtogether0002243001&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101}'
+        ,
         {
           withCredentials:true  // 쿠키 cors 통신 설정
-      }).then(response => {
-                  console.log(response);
-      });
-      
+        }).then(response => {
+            console.log(response);
+          });
     }  
 
 
